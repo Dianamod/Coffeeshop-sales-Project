cd52bd9deed0ac17d85b24fca42d4b25b85c44bf
Dianamod <diana.modyrko@gmail.com>
2024-08-29 13:42:59 +0300

    Update csv to sql.py

--- csv to sql.py
+++ csv to sql.py
@@ -6,9 +6,9 @@ from sqlalchemy import create_engine
 # Authenticate with Kaggle
 kaggle.api.authenticate()
 kaggle.api.dataset_download_files(
-    'tmthyjames/nashville-housing-data', path='.', unzip=True)
+    'ahmedabbas757/coffee-sales', path='.', unzip=True)
 
-file_path = 'Nashville_housing_data_2013_2016.csv'
+file_path = 'Coffee Shop Sales.csv'
 df = pd.read_csv(file_path)
 print(df.head())
 
@@ -16,11 +16,11 @@ engine = create_engine(
     "mysql+mysqlconnector://root:Dianatsubera_1991!@localhost:3306/test")
 
 
-file_path = 'Nashville_housing_data_2013_2016.csv'
+file_path = 'Coffee Shop Sales.csv'
 df = pd.read_csv(file_path)
 
 try:
-    df.to_sql(name='Nashville_housing_data_2013_2016.csv', con=engine,
+    df.to_sql(name='Coffee Shop Sales.csv', con=engine,
               if_exists='append', index=False)
     print('data inserted successful')
 except Exception as e:
